6ecfb47edb0d776966909eb20d40f14db0d16817
John <51002088+jciii91@users.noreply.github.com>
2019-08-14 15:58:14 -0400

    Create WAR Card Database - Uncommon Run B.js

---
+++ WAR Card Database - Uncommon Run B.js
@@ -0,0 +1,49 @@
+let uncommonRunB = new Array();
+    uncommonRunB[0] = {
+        name: "Emergence Zone",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461172&type=card",
+        cmc: 0,
+        type: "Land",
+        subtype: undefined,
+        set: "WAR",
+        rules: "Tap: Add 1 Colorless Mana. 1, Tap, Sacrifice Emergence Zone: You may cast spells this turn as though they had flash.",
+        flavor: "The Planar Bridge opened over the Chamber of the Guildpact, reducing the symbol of Ravnica's endurance to rubble.",
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[1] = {
+        name: "Firemind Vessel",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461164&type=card",
+        cmc: 4,
+        type: "Artifact",
+        subtype: undefined,
+        set: "WAR",
+        rules: "Firemind Vessel enters the battlefield tapped. Tap: Add two mana of different colors.",
+        flavor: "The city mourned Niv-Mizzet's death, but he left behind the components for his rebirth.",
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[2] = {
+        name: "Neoform",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461133&type=card",
+        cmc: GU,
+        type: "Sorcery",
+        subtype: undefined,
+        set: "WAR",
+        rules: "As an additional cost to cast this spell, sacrifice a creature. Search your library for a creature card with converted mana cost equal to 1 plus the sacrificed creature's converted mana cost, put that card onto the battlefield with an additional +1/+1 counter on it, then shuffle your library.",
+        flavor: undefined,
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[3] = {
+        name: "Devouring Hellion",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461051&type=card",
+        cmc: 2R,
+        type: "Creature",
+        subtype: "Hellion",
+        set: "WAR",
+        rules: "As Devouring Hellion enters the battlefield, you may sacrifice any number of creatures and/or planeswalkers. If you do, it enters with twice that many +1/+1 counters on it.",
+        flavor: "There's no crisis a hellion can't make worse.",
+        power: 2,
+        toughness: 2
+    }
