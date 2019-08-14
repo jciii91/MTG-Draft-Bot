fed1f07588fb40597458e3a4e64f7dfdc265ccfe
John <51002088+jciii91@users.noreply.github.com>
2019-08-14 17:25:08 -0400

    Update WAR Card Database - Uncommon Run B.js

--- WAR Card Database - Uncommon Run B.js
+++ WAR Card Database - Uncommon Run B.js
@@ -26,7 +26,7 @@ let uncommonRunB = new Array();
     uncommonRunB[2] = {
         name: "Neoform",
         img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461133&type=card",
-        cmc: GU,
+        cmc: "GU",
         type: "Sorcery",
         subtype: undefined,
         set: "WAR",
@@ -38,7 +38,7 @@ let uncommonRunB = new Array();
     uncommonRunB[3] = {
         name: "Devouring Hellion",
         img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461051&type=card",
-        cmc: 2R,
+        cmc: "2R",
         type: "Creature",
         subtype: "Hellion",
         set: "WAR",
@@ -46,4 +46,172 @@ let uncommonRunB = new Array();
         flavor: "There's no crisis a hellion can't make worse.",
         power: 2,
         toughness: 2
+    }
+    uncommonRunB[4] = {
+        name: "Rally of Wings",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=460954&type=card",
+        cmc: "1W",
+        type: "Instant",
+        subtype: undefined,
+        set: "WAR",
+        rules: "Untap all creatures you control. Creatures you control with flying get +2/+2 until end of turn.",
+        flavor: "Even the clouds became a field of battle. Known as the Sky Theater, it saw the Eternals clash with squadrons of angels.",
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[5] = {
+        name: "Gleaming Overseer",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461125&type=card",
+        cmc: "1UB",
+        type: "Creature",
+        subtype: "Zombie Wizard",
+        set: "WAR",
+        rules: "When Gleaming Overseer enters the battlefield, amass 1. (Put a +1/+1 counter on an Army you control. If you don't control one, create a 0/0 black Zombie Army creature token first.) Zombie tokens you control have hexproof and menace.",
+        flavor: undefined,
+        power: 1,
+        toughness: 4
+    }
+    uncommonRunB[6] = {
+        name: "Bolt Bend",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461042&type=card",
+        cmc: "3R",
+        type: "Instant",
+        subtype: undefined,
+        set: "WAR",
+        rules: "This spell costs 3 less to cast if you control a creature with power 4 or greater. Change the target of target spell or ability with a single target.",
+        flavor: "\"Give them everything you've got . . . and some things you don't.\"—Ral Zarek",
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[7] = {
+        name: "Ugin's Conjurant",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=460930&type=card",
+        cmc: 0,
+        type: "Creature",
+        subtype: "Spirit Monk",
+        set: "WAR",
+        rules: "Ugin's Conjurant enters the battlefield with X +1/+1 counters on it. If damage would be dealt to Ugin's Conjurant while it has a +1/+1 counter on it, prevent that damage and remove that many +1/+1 counters from Ugin's Conjurant.",
+        flavor: undefined,
+        power: 0,
+        toughness: 0
+    }
+    uncommonRunB[8] = {
+        name: "Price of Betrayal",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461029&type=card",
+        cmc: "B",
+        type: "Sorcery",
+        subtype: undefined,
+        set: "WAR",
+        rules: "Remove up to five counters from target artifact, creature, planeswalker, or opponent.",
+        flavor: "The moment Liliana defied Nicol Bolas, her contract was broken, and her life was forfeit. But she was free to choose her fate, and she decided it was worth the price.",
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[9] = {
+        name: "Elite Guardmage",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461122&type=card",
+        cmc: "2WU",
+        type: "Creature",
+        subtype: "Human Wizard",
+        set: "WAR",
+        rules: "Flying. When Elite Guardmage enters the battlefield, you gain 3 life and draw a card.",
+        flavor: "\"Be careful. You'll have more than Dovin to contend with if you hope to breach New Prahv.\"—Lavinia, to Chandra Nalaar",
+        power: 2,
+        toughness: 3
+    }
+    uncommonRunB[10] = {
+        name: "Dreadhorde Twins",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461053&type=card",
+        cmc: "3R",
+        type: "Creature",
+        subtype: "Zombie Jackal Warrior",
+        set: "WAR",
+        rules: "When Dreadhorde Twins enters the battlefield, amass 2. (Put two +1/+1 counters on an Army you control. If you don't control one, create a 0/0 black Zombie Army creature token first.) Zombie tokens you control have trample.",
+        flavor: undefined,
+        power: 2,
+        toughness: 2
+    }
+    uncommonRunB[11] = {
+        name: "Lazotep Plating",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=460986&type=card",
+        cmc: "1U",
+        type: "Instant",
+        subtype: undefined,
+        set: "WAR",
+        rules: "Amass 1. (Put a +1/+1 counter on an Army you control. If you don't control one, create a 0/0 black Zombie Army creature token first.). You and permanents you control gain hexproof until end of turn. (You and they can't be the targets of spells or abilities your opponents control.)",
+        flavor: undefined,
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[12] = {
+        name: "Tenth District Legionnaire",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461149&type=card",
+        cmc: "RW",
+        type: "Creature",
+        subtype: "Human Soldier",
+        set: "WAR",
+        rules: "Haste. Whenever you cast a spell that targets Tenth District Legionnaire, put a +1/+1 counter on Tenth District Legionnaire, then scry 1.",
+        flavor: "\"The Tenth is under heavy attack, but it's being handled. I sent Mileva.\"—Tajic",
+        power: 2,
+        toughness: 2
+    }
+    uncommonRunB[13] = {
+        name: "Challenger Troll",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461084&type=card",
+        cmc: "4G",
+        type: "Creature",
+        subtype: "Troll",
+        set: "WAR",
+        rules: "Each creature you control with power 4 or greater can't be blocked by more than one creature.",
+        flavor: "For most, war is a calamity. For some, an opportunity. And for the very few, a pleasure.",
+        power: 6,
+        toughness: 5
+    }
+    uncommonRunB[14] = {
+        name: "Rescuer Sphinx",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=460992&type=card",
+        cmc: "2UU",
+        type: "Creature",
+        subtype: "Sphinx",
+        set: "WAR",
+        rules: "Flying. As Rescuer Sphinx enters the battlefield, you may return a nonland permanent you control to its owner's hand. If you do, Rescuer Sphinx enters the battlefield with a +1/+1 counter on it.",
+        flavor: "\"Now, little fighter, show me you deserved this second chance.\"",
+        power: 3,
+        toughness: 2
+    }
+    uncommonRunB[15] = {
+        name: "Interplanar Beacon",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461174&type=card",
+        cmc: 0,
+        type: "Land",
+        subtype: undefined,
+        set: "WAR",
+        rules: "Whenever you cast a planeswalker spell, you gain 1 life. Tap: Add 1 Colorless mana. 1, Tap: Add two mana of different colors. Spend this mana only to cast planeswalker spells.",
+        flavor: undefined,
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[16] = {
+        name: "Tyrant's Scorn",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461152&type=card",
+        cmc: "UB",
+        type: "Instant",
+        subtype: undefined,
+        set: "WAR",
+        rules: "Choose one — Destroy target creature with converted mana cost 3 or less -OR- Return target creature to its owner's hand.",
+        flavor: "\"The hero with the magic sword slays the dragon? Not this time.\"—Nicol Bolas",
+        power: undefined,
+        toughness: undefined
+    }
+    uncommonRunB[17] = {
+        name: "Eternal Taskmaster",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461017&type=card",
+        cmc: "1B",
+        type: "Creature",
+        subtype: "Zombie",
+        set: "WAR",
+        rules: "Eternal Taskmaster enters the battlefield tapped. Whenever Eternal Taskmaster attacks, you may pay 2B. If you do, return target creature card from your graveyard to your hand.",
+        flavor: "\"They are called Eternals. They will never stop.\"—Jace Beleren",
+        power: 2,
+        toughness: 3
     }
