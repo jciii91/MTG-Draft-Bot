d4b3b0d4992aaab0fcb2cf92e51eabeb609b5f71
John <51002088+jciii91@users.noreply.github.com>
2019-08-20 17:05:32 -0400

    Create WAR Card Database - Uncommon Run PW.js

---
+++ WAR Card Database - Uncommon Run PW.js
@@ -0,0 +1,13 @@
+let uncommonRunPW = new Array();
+    uncommonRunA[0] = {
+        name: "Evolution Sage",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=461086&type=card",
+        cmc: "2G",
+        type: "Creature",
+        subtype: "Elf Druid",
+        set: "WAR",
+        rules: "Whenever a land enters the battlefield under your control, proliferate. (Choose any number of permanents and/or players, then give each another counter of each kind already there.)",
+        flavor: "\"To care for yourself, cultivate the world. To care for the world, cultivate yourself.\"",
+        power: 3,
+        toughness: 2
+    }
