0566b54577f64b762dc8e5b658e0dcdf9b49801e
John <51002088+jciii91@users.noreply.github.com>
2019-08-20 23:56:09 -0400

    Create WAR Card Database - Rares and Mythic Rares.js

---
+++ WAR Card Database - Rares and Mythic Rares.js
@@ -0,0 +1,13 @@
+let rareRun = new Array();
+    rareRun[0] = {
+        name: "Kasmina's Transmutation",
+        img: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=460984&type=card",
+        cmc: "1U",
+        type: "Enchantment",
+        subtype: "Aura",
+        set: "WAR",
+        rules: "Enchant creature. Enchanted creature loses all abilities and has base power and toughness 1/1.",
+        flavor: "\"Hop away now, little Eternal. Go eat some flies.\" — Kasmina",
+        power: undefined,
+        toughness: undefined
+    }
