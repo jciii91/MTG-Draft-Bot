b44c44a6f16dd4bca2296f652cc33fc75422eeca
John <51002088+jciii91@users.noreply.github.com>
2019-08-22 17:12:47 -0400

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -26,12 +26,12 @@ function rareTest(x) {
 	let check = true;
 	if (x <= 75) {
 		while (check) {
-			rare = rareRun[(rng(73)-1)];
+			rare = rareRun[(rng(121)-1)];
 			check = rare.type.includes("Planeswalker");
 		}
 	} else if (x > 75) {
 		while (check) {
-			rare = rareRun[(rng(73)-1)];
+			rare = rareRun[(rng(121)-1)];
 			check = !(rare.type.includes("Planeswalker"));
 		}
 	}
@@ -43,26 +43,27 @@ function rareTest(x) {
 //If an Uncommon planeswalker appears, ensures the rare/mythic is not a Planeswalker
 
 function uncommonDeterminant() {
-	let numDet = rng(100);
+	let numDet1 = rng(100);
+	let numDet2 = rng(100);
 	
-	if ((numDet <= 75) && (numDet > 50)){
+	if ((numDet1 <= 75) && (numDet2 > 50)){
 		rareTest(numDet);
 		show_image(uncommonRunA[rng(54)-1].img, 265, 370);
 		show_image(uncommonRunB[rng(66)-1].img, 265, 370);
 		show_image(uncommonRunPW[rng(20)-1].img, 265, 370);
-	} else if ((numDet <= 75) && (numDet <= 50)){
+	} else if ((numDet1 <= 75) && (numDet2 <= 50)){
 		rareTest(numDet);
 		let B = rng(66)-1;
 		show_image(uncommonRunB[B].img, 265, 370);
 		show_image(uncommonRunB[(B == 65 ? 0 : B+1)].img, 265, 370);
 		show_image(uncommonRunPW[rng(20)-1].img, 265, 370);
-	} else if ((numDet > 75) && (numDet > 50)){
+	} else if ((numDet1 > 75) && (numDet2 > 50)){
 		rareTest(numDet);
 		let A = rng(54)-1;
 		show_image(uncommonRunA[A].img, 265, 370);
 		show_image(uncommonRunA[(A == 53 ? 0 : A+1)].img, 265, 370);
 		show_image(uncommonRunB[rng(66)-1].img, 265, 370);
-	} else if ((numDet > 75) && (numDet <= 50)){
+	} else if ((numDet1 > 75) && (numDet2 <= 50)){
 		rareTest(numDet);
 		let B = rng(66)-1;
 		show_image(uncommonRunA[rng(53)-1].img, 265, 370);
