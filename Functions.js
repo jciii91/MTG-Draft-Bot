91ec02dfd0549c34213e0402bce880a7095cde25
John <51002088+jciii91@users.noreply.github.com>
2019-09-05 16:20:11 -0400

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -64,6 +64,7 @@ function pickCard(cardID) {
 		startDraft();
 		pickCounter = 1;
 	}
+	alert(pickCounter);
 	alert(pack.length);
 	var element = document.getElementById("currentPack");
 	var card = document.getElementById(cardID);
