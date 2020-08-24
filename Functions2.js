b407269ed9ca392dfc777375b17b11a191d0e091
John <51002088+jciii91@users.noreply.github.com>
2020-08-24 12:42:22 -0400

    Update Functions2.js
    Added Common Run B, added loops to makePack() for Common Run B.

--- Functions2.js
+++ Functions2.js
@@ -24,6 +24,16 @@ let commonRunA = [
 	"Naga Eternal", "Goblin Assailant", "Loxodon Sergeant"
 	];
 
+//Common Run B
+let commonRunB = [
+	"Return to Nature", "Shriekdiver", "Snarespinner", "Toll of the Invasion", "Kraul Stinger", "Lazotep Behemoth", "Wardscale Crocodile", "Duskmantle Operative", "Vivien's Grizzly", "Spark Reaper", "Courage in Crisis",
+	"Sorin's Thirst", "Forced Landing", "Vampire Opportunist", "Steady Aim", "Aid the Fallen", "Return to Nature", "Charity Extractor", "New Horizons", "Toll of the Invasion", "Primordial Wurm", "Lazotep Behemoth",
+	"Kronch Wrangler", "Kaya's Ghostform", "Snarespinner", "Tithebearer Giant", "Wardscale Crocodile", "Spark Reaper", "Forced Landing", "Shriekdiver", "Courage in Crisis", "Sorin's Thirst", "Kraul Stinger",
+	"Vampire Opportunist", "Vivien's Grizzly", "Duskmantle Operative", "Snarespinner", "Toll of the Invasion", "Kronch Wrangler", "Aid the Fallen", "Steady Aim", "Charity Extractor", "Primordial Wurm", "Lazotep Behemoth",
+	"Return to Nature", "Kaya's Ghostform", "New Horizons", "Tithebearer Giant", "Vivien's Grizzly", "Spark Reaper", "Courage in Crisis", "Shriekdiver", "Wardscale Crocodile", "Duskmantle Operative", "Forced Landing",
+	"Sorin's Thirst", "Kraul Stinger", "Vampire Opportunist", "Steady Aim", "Charity Extractor", "Kronch Wrangler", "Tithebearer Giant", "New Horizons", "Aid the Fallen", "Primordial Wurm", "Kaya's Ghostform"
+	];
+
 //Random number generator
 
 function rng(max) {
@@ -32,222 +42,83 @@ function rng(max) {
   return Math.floor(rand);
 }
 
-function makePack() {
-	
+function makePack() {													//Function to generate a pack. Typically called 8 times in a single session.
+
 	randomPack = new Array();
-	let packSeed = rng(5);
-	
+	let packSeed = rng(5);												//Call rng() to decide on the pack's seed, there are 5 possible types of pack.
+																		//Each seed uses 3 'for' loops, one for each type of common in the pack.
 	if (packSeed === 1) {
 		
 		let index = rng(66)-1;
-		for (i=0; i<2; i++) {
+		for (i=0; i<2; i++) {											//Loop to determine commons from run A
 			randomPack[i] = commonRunA[index];
 			index = (index === commonRunA.length-1) ? 0 : index + 1;
+		}
+		
+		index = rng(66)-1;
+		for (i=2; i<4; i++) {											//Loop to determine commons from run B
+			randomPack[i] = commonRunB[index];
+			index = (index === commonRunB.length-1) ? 0 : index + 1;
 		}
 
 	} else if (packSeed === 2) {
 		
 		let index = rng(66)-1;
-		for (i=0; i<3; i++) {
+		for (i=0; i<3; i++) {											//Loop to determine commons from run A
 			randomPack[i] = commonRunA[index];
 			index = (index === commonRunA.length-1) ? 0 : index + 1;
+		}
+		
+		index = rng(66)-1;
+		for (i=3; i<5; i++) {											//Loop to determine commons from run B
+			randomPack[i] = commonRunB[index];
+			index = (index === commonRunB.length-1) ? 0 : index + 1;
 		}
 		
 	} else if (packSeed === 3) {
 
 		let index = rng(66)-1;
-		for (i=0; i<4; i++) {
+		for (i=0; i<4; i++) {											//Loop to determine commons from run A
 			randomPack[i] = commonRunA[index];
 			index = (index === commonRunA.length-1) ? 0 : index + 1;
+		}
+		
+		index = rng(66)-1;
+		for (i=4; i<6; i++) {											//Loop to determine commons from run B
+			randomPack[i] = commonRunB[index];
+			index = (index === commonRunB.length-1) ? 0 : index + 1;
 		}
 
 	} else if (packSeed === 4) {
 
 		let index = rng(66)-1;
-		for (i=0; i<4; i++) {
+		for (i=0; i<4; i++) {											//Loop to determine commons from run A
 			randomPack[i] = commonRunA[index];
 			index = (index === commonRunA.length-1) ? 0 : index + 1;
+		}
+		
+		index = rng(66)-1;
+		for (i=4; i<7; i++) {											//Loop to determine commons from run B
+			randomPack[i] = commonRunB[index];
+			index = (index === commonRunB.length-1) ? 0 : index + 1;
 		}
 
 	} else if (packSeed === 5) {
 
 		let index = rng(66)-1;
-		for (i=0; i<4; i++) {
+		for (i=0; i<4; i++) {											//Loop to determine commons from run A
 			randomPack[i] = commonRunA[index];
 			index = (index === commonRunA.length-1) ? 0 : index + 1;
 		}
-	}	
-	alert(randomPack[1]);
-}
-
-/* Old Pack generator
-
-function makePack() {
-	randomPack = new Array();
-	uncommonDeterminant();
-	
-	let packNum = rng(5);
-
-	if (packNum === 1) {
-
-		let A1 = rng(67)-1; //Determines the first card selected
-		randomPack[4] = commonRunA[A1];
-
-		let A2 = (A1 === 66) ? 0 : A1 + 1;
-		randomPack[5] = commonRunA[A2];
-
-		let B1 = rng(66)-1; //Determines the first card selected
-		randomPack[6] = commonRunB[B1];
-
-		let B2 = (B1 === 65) ? 0 : B1 + 1;
-		randomPack[7] = commonRunB[B2];
-
-		let C1 = rng(55)-1; //Determines the first card selected
-		randomPack[8] = commonRunC1[C1];
-
-		let C2 = (C1 === 54) ? 0 : C1 + 1;
-		randomPack[9] = commonRunC1[C2];
-
-		let C3 = (C2 === 54) ? 0 : C2 + 1;
-		randomPack[10] = commonRunC1[C3];
-
-		let C4 = (C3 === 54) ? 0 : C3 + 1;
-		randomPack[11] = commonRunC1[C4];
-
-		let C5 = (C4 === 54) ? 0 : C4 + 1;
-		randomPack[12] = commonRunC1[C5];
-
-		let C6 = (C5 === 54) ? 0 : C5 + 1;
-		randomPack[13] = commonRunC1[C6];
-
-	} else if (packNum === 2) {
 		
-		let A1 = rng(67)-1; //Determines the first card selected
-		randomPack[4] = commonRunA[A1];
-
-		let A2 = (A1 === 66) ? 0 : A1 + 1;
-		randomPack[5] = commonRunA[A2];
-
-		let A3 = (A2 === 66) ? 0 : A2 + 1;
-		randomPack[6] = commonRunA[A3];
-
-		let B1 = rng(66)-1; //Determines the first card selected
-		randomPack[7] = commonRunB[B1];
-
-		let B2 = (B1 === 65) ? 0 : B1 + 1;
-		randomPack[8] = commonRunB[B2];
-
-		let C1 = rng(55)-1; //Determines the first card selected
-		randomPack[9] = commonRunC1[C1];
-
-		let C2 = (C1 === 54) ? 0 : C1 + 1;
-		randomPack[10] = commonRunC1[C2];
-
-		let C3 = (C2 === 54) ? 0 : C2 + 1;
-		randomPack[11] = commonRunC1[C3];
-
-		let C4 = (C3 === 54) ? 0 : C3 + 1;
-		randomPack[12] = commonRunC1[C4];
-
-		let C5 = (C4 === 54) ? 0 : C4 + 1;
-		randomPack[13] = commonRunC1[C5];
-
-	} else if (packNum === 3) {
-
-		let A1 = rng(67)-1; //Determines the first card selected
-		randomPack[4] = commonRunA[A1];
-
-		let A2 = (A1 === 66) ? 0 : A1 + 1;
-		randomPack[5] = commonRunA[A2];
-
-		let A3 = (A2 === 66) ? 0 : A2 + 1;
-		randomPack[6] = commonRunA[A3];
-
-		let A4 = (A3 === 66) ? 0 : A3 + 1;
-		randomPack[7] = commonRunA[A4];
-
-		let B1 = rng(66)-1; //Determines the first card selected
-		randomPack[8] = commonRunB[B1];
-
-		let B2 = (B1 === 65) ? 0 : B1 + 1;
-		randomPack[9] = commonRunB[B2];
-
-		let C1 = rng(55)-1; //Determines the first card selected
-		randomPack[10] = commonRunC2[C1];
-
-		let C2 = (C1 === 54) ? 0 : C1 + 1;
-		randomPack[11] = commonRunC2[C2];
-
-		let C3 = (C2 === 54) ? 0 : C2 + 1;
-		randomPack[12] = commonRunC2[C3];
-
-		let C4 = (C3 === 54) ? 0 : C3 + 1;
-		randomPack[13] = commonRunC2[C4];
-
-	} else if (packNum === 4) {
-
-		let A1 = rng(67)-1; //Determines the first card selected
-		randomPack[4] = commonRunA[A1];
-
-		let A2 = (A1 === 66) ? 0 : A1 + 1;
-		randomPack[5] = commonRunA[A2];
-
-		let A3 = (A2 === 66) ? 0 : A2 + 1;
-		randomPack[6] = commonRunA[A3];
-
-		let A4 = (A3 === 66) ? 0 : A3 + 1;
-		randomPack[7] = commonRunA[A4];
-
-		let B1 = rng(66)-1; //Determines the first card selected
-		randomPack[8] = commonRunB[B1];
-
-		let B2 = (B1 === 65) ? 0 : B1 + 1;
-		randomPack[9] = commonRunB[B2];
-
-		let B3 = (B2 === 65) ? 0 : B2 + 1;
-		randomPack[10] = commonRunB[B3];
-
-		let C1 = rng(55)-1; //Determines the first card selected
-		randomPack[11] = commonRunC2[C1];
-
-		let C2 = (C1 === 54) ? 0 : C1 + 1;
-		randomPack[12] = commonRunC2[C2];
-
-		let C3 = (C2 === 54) ? 0 : C2 + 1;
-		randomPack[13] = commonRunC2[C3];
-
-	} else if (packNum === 5) {
-
-		let A1 = rng(67); //Determines the first card selected
-		randomPack[4] = commonRunA[A1];
-
-		let A2 = (A1 === 66) ? 0 : A1 + 1;
-		randomPack[5] = commonRunA[A2];
-
-		let A3 = (A2 === 66) ? 0 : A2 + 1;
-		randomPack[6] = commonRunA[A3];
-
-		let A4 = (A3 === 66) ? 0 : A3 + 1;
-		randomPack[7] = commonRunA[A4];
-
-		let B1 = rng(66)-1; //Determines the first card selected
-		randomPack[8] = commonRunB[B1];
-
-		let B2 = (B1 === 65) ? 0 : B1 + 1;
-		randomPack[9] = commonRunB[B2];
-
-		let B3 = (B2 === 65) ? 0 : B2 + 1;
-		randomPack[10] = commonRunB[B3];
-
-		let B4 = (B3 === 65) ? 0 : B3 + 1;
-		randomPack[11] = commonRunB[B4];
-
-		let C1 = rng(55)-1; //Determines the first card selected
-		randomPack[12] = commonRunC2[C1];
-
-		let C2 = (C1 === 54) ? 0 : C1 + 1;
-		randomPack[13] = commonRunC2[C2];
-
-	}
-
-}*/
+		index = rng(66)-1;
+		for (i=4; i<8; i++) {											//Loop to determine commons from run B
+			randomPack[i] = commonRunB[index];
+			index = (index === commonRunB.length-1) ? 0 : index + 1;
+		}
+	}	
+	alert(randomPack[0]);
+	alert(randomPack[1]);
+	alert(randomPack[2]);
+	alert(randomPack[3]);
+}
