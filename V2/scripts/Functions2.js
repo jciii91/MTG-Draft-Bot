74600701cac3aa7c821b2a306f7ed9cc52a0e2f0
JCIII <jcostello711@gmail.com>
2021-12-26 10:38:10 -0500

    findCard implemented, fillPlayerPacks now writes card objects for each entry in cardPool.

--- V2/scripts/Functions2.js
+++ V2/scripts/Functions2.js
@@ -13,111 +13,6 @@ function show_image(src, num) {
     document.getElementById("currentPack").appendChild(img);
 }
 
-//Common Run A
-let commonRunA = [
-	"Kasmina's Transmutation", "Goblin Assault Team", "Ironclad Krovod",
-	"Spellkeeper Weird", "Ahn-Crop Invader", "War Screecher",
-	"Ashiok's Skulker", "Invading Manticore", "Bulwark Giant",
-	"Kiora's Dambreaker", "Raging Kronch", "Gideon's Sacrifice",
-	"Teferi's Time Twist", "Grim Initiate", "Martyr for the Cause",
-	"Contentious Plan", "Blindblast", "Battlefield Promotion",
-	"Crush Dissent", "Chainwhip Cyclops", "Teyo's Lightshield",
-	"No Escape", "Turret Ogre", "Ironclad Krovod",
-	"Ashiok's Skulker", "Honor the God-Pharaoh", "Rising Populace",
-	"Stealth Mission", "Goblin Assault Team", "Pouncing Lynx",
-	"Sky Theater Strix", "Goblin Assailant", "Makeshift Battalion",
-	"Kasmina's Transmutation", "Samut's Sprint", "War Screecher",
-	"Teferi's Time Twist", "Ahn-Crop Invader", "Bulwark Giant",
-	"Crush Dissent", "Invading Manticore", "Gideon's Sacrifice",
-	"Naga Eternal", "Raging Kronch", "Battlefield Promotion",
-	"Sky Theater Strix", "Blindblast", "Loxodon Sergeant",
-	"Kiora's Dambreaker", "Chainwhip Cyclops", "Teyo's Lightshield",
-	"Spellkeeper Weird", "Turret Ogre", "Pouncing Lynx",
-	"Stealth Mission", "Honor the God-Pharaoh", "Rising Populace",
-	"No Escape", "Grim Initiate", "Makeshift Battalion",
-	"Contentious Plan", "Samut's Sprint", "Martyr for the Cause",
-	"Naga Eternal", "Goblin Assailant", "Loxodon Sergeant"
-	];
-
-//Common Run B
-let commonRunB = [
-	"Return to Nature", "Shriekdiver", "Snarespinner", "Toll of the Invasion", "Kraul Stinger", "Lazotep Behemoth", "Wardscale Crocodile", "Duskmantle Operative", "Vivien's Grizzly", "Spark Reaper", "Courage in Crisis",
-	"Sorin's Thirst", "Forced Landing", "Vampire Opportunist", "Steady Aim", "Aid the Fallen", "Return to Nature", "Charity Extractor", "New Horizons", "Toll of the Invasion", "Primordial Wurm", "Lazotep Behemoth",
-	"Kronch Wrangler", "Kaya's Ghostform", "Snarespinner", "Tithebearer Giant", "Wardscale Crocodile", "Spark Reaper", "Forced Landing", "Shriekdiver", "Courage in Crisis", "Sorin's Thirst", "Kraul Stinger",
-	"Vampire Opportunist", "Vivien's Grizzly", "Duskmantle Operative", "Snarespinner", "Toll of the Invasion", "Kronch Wrangler", "Aid the Fallen", "Steady Aim", "Charity Extractor", "Primordial Wurm", "Lazotep Behemoth",
-	"Return to Nature", "Kaya's Ghostform", "New Horizons", "Tithebearer Giant", "Vivien's Grizzly", "Spark Reaper", "Courage in Crisis", "Shriekdiver", "Wardscale Crocodile", "Duskmantle Operative", "Forced Landing",
-	"Sorin's Thirst", "Kraul Stinger", "Vampire Opportunist", "Steady Aim", "Charity Extractor", "Kronch Wrangler", "Tithebearer Giant", "New Horizons", "Aid the Fallen", "Primordial Wurm", "Kaya's Ghostform"
-	];
-
-//Commmon Run C1
-let commonRunC1 = [
-	"Saheeli's Silverwing", "Bloom Hulk", "Charmed Stray", "Aven Eternal", "Burning Prophet", "Ob Nixilis's Cruelty", "Jaya's Greeting", "Totally Lost", "Centaur Nurturer", "Iron Bully", "Erratic Visionary", "Enforcer Griffin",
-	"Lazotep Reaver", "Demolish", "Law-Rune Enforcer", "Defiant Strike", "Mana Geode", "Thunder Drake", "Charmed Stray", "Vraska's Finisher", "Heartfire", "Pollenbright Druid", "Guild Globe", "Chandra's Pyrohelix", "Davriel's Shadowfugue",
-	"Wanderer's Strike", "Relentless Advance", "Enforcer Griffin", "Jaya's Greeting", "Unlikely Aid", "Aven Eternal", "Sarkhan's Catharsis", "Bloom Hulk", "Saheeli's Silverwing", "Ob Nixilis's Cruelty", "Totally Lost",
-	"Law-Rune Enforcer", "Burning Prophet", "Relentless Advance", "Centaur Nurturer", "Demolish", "Lazotep Reaver", "Defiant Strike", "Erratic Visionary", "Guild Globe", "Chandra's Pyrohelix", "Unlikely Aid", "Wanderer's Strike",
-	"Heartfire", "Mana Geode", "Pollenbright Druid", "Davriel's Shadowfugue", "Thunder Drake", "Sarkhan's Catharsis", "Vraska's Finisher"
-	]
-	
-//Commmon Run C2
-let commonRunC2 = [
-	"Spellgorger Weird", "Arboreal Grazer", "Herald of the Dreadhorde", "Prismite", "Giant Growth", "Spark Harvest", "Arboreal Grazer", "Divine Arrow", "Wall of Runes", "Arlinn's Wolf", "Iron Bully", "Callous Dismissal",
-	"Nahiri's Stoneblades", "Band Together", "Prismite", "Herald of the Dreadhorde", "Thundering Ceratok", "Wall of Runes", "Spark Harvest", "Topple the Statue", "Giant Growth", "Spellgorger Weird", "Tamiyo's Epiphany",
-	"Banehound", "Gateway Plaza", "Band Together", "Trusted Pegasus", "Arlinn's Wolf", "Nahiri's Stoneblades", "Callous Dismissal", "Arboreal Grazer", "Divine Arrow", "Thundering Ceratok", "Spellgorger Weird", "Topple the Statue",
-	"Herald of the Dreadhorde", "Gateway Plaza", "Tamiyo's Epiphany", "Trusted Pegasus", "Banehound", "Giant Growth", "Topple the Statue", "Callous Dismissal", "Gateway Plaza", "Arlinn's Wolf", "Wall of Runes", "Divine Arrow",
-	"Prismite", "Spark Harvest", "Thundering Ceratok", "Nahiri's Stoneblades", "Tamiyo's Epiphany", "Banehound", "Trusted Pegasus", "Band Together"
-	]
-
-//Uncommon Run A
-let uncommonRunA = [
-	"Evolution Sage", "Merfolk Skydiver", "Eternal Skylord", "Cruel Celebrant", "Bond of Passion", "Deathsprout", "Ajani's Pridemate", "Mayhem Devil", "Paradise Druid", "Chandra's Triumph", "Rubblebelt Rioters",
-	"Bond of Discipline", "Huatli's Raptor", "Dreadmalkin", "Leyline Prowler", "Ral's Outburst", "Augur of Bolas", "Tibalt's Rager", "Despark", "Sunblade Angel", "Bleeding Edge", "Merfolk Skydiver", "Storm the Citadel", "Domri's Ambush",
-	"Vizier of the Scorpian", "Cruel Celebrant", "Jace's Triumph", "Prison Realm", "Mayhem Devil", "Cyclops Electromancer", "Ajani's Pridemate", "Deathsprout", "Dreadmalkin", "Rubblebelt Rioters", "Evolution Sage",
-	"Bond of Passion", "Eternal Skylord", "Huatli's Raptor", "Bleeding Edge", "Tibalt's Rager", "Leyline Prowler", "Paradise Druid", "Ral's Outburst", "Bond of Discipline", "Vizier of the Scorpian", "Chandra's Triumph",
-	"Augur of Bolas", "Storm the Citadel", "Sunblade Angel", "Domri's Ambush", "Jace's Triumph", "Cyclops Electromancer", "Despark", "Prison Realm"
-	]
-	
-//Uncommon Run B
-let uncommonRunB = [
-	"Emergence Zone", "Firemind Vessel", "Neoform", "Devouring Hellion", "Rally of Wings", "Gleaming Overseer", "Bolt Bend", "Ugin's Conjurant", "Price of Betrayal", "Elite Guardmage", "Dreadhorde Twins", "Lazotep Plating",
-	"Tenth District Legionnaire", "Challenger Troll", "Rescuer Sphinx", "Interplanar Beacon", "Tyrant's Scorn", "Eternal Taskmaster", "Mowu, Loyal Companion", "Dovin's Veto", "Grateful Apparition", "Angrath's Rampage",
-	"Emergence Zone", "Liliana's Triumph", "Bond of Flourishing", "Heartwarming Redemption", "Nissa's Triumph", "Bond of Insight", "Pledge of Unity", "Firemind Vessel", "Bond of Revival", "Challenger Troll", "Invade the City",
-	"Flux Channeler", "Eternal Taskmaster", "God-Pharaoh's Statue", "Gideon's Triumph", "Neoform", "Devouring Hellion", "Gleaming Overseer", "Ugin's Conjurant", "Rally of Wings", "Rescuer Sphinx", "Tenth District Legionnaire",
-	"Dovin's Veto", "Mowu, Loyal Companion", "Price of Betrayal", "Elite Guardmage", "Lazotep Plating", "Heartwarming Redemption", "Bolt Bend", "Bond of Insight", "Interplanar Beacon", "Tyrant's Scorn", "Bond of Flourishing",
-	"Bond of Revival", "Grateful Apparition", "Flux Channeler", "Dreadhorde Twins", "Nissa's Triumph", "Invade the City", "Liliana's Triumph", "Angrath's Rampage", "God-Pharaoh's Statue", "Gideon's Triumph", "Pledge of Unity"
-	]
-	
-//Uncommon Planeswalkers Run
-let uncommonRunPW = [
-	"Angrath, Captain of Chaos", "Arlinn, Voice of the Pack", "Ashiok, Dream Render", "Davriel, Rogue Shadowmage", "Dovin, Hand of Control", "Huatli, the Sun's Heart", "Jaya, Venerated Firemage", "Jiang Yanggu, Wildcrafter",
-	"Kasmina, Enigmatic Mentor", "Kaya, Bane of the Dead", "Kiora, Behemoth Beckoner", "Nahiri, Storm of Stone", "Narset, Parter of Veils", "Ob Nixilis, the Hate-Twisted", "Saheeli, Sublime Artificer", "Samut, Tyrant Smasher",
-	"Teyo, the Shieldmage", "The Wanderer", "Tibalt, Rakish Instigator", "Vraska, Swarm's Eminence"
-	]
-	
-//Rares and Mythic Rares
-let rares = [
-	"Awakening of Vitu-Ghazi", "Bioessence Hydra", "Blast Zone", "Bolas's Citadel", "Casualties of War", "Command the Dreadhorde", "Commence the Endgame",
-	"Deliver Unto Evil", "Dreadhorde Arcanist", "Dreadhorde Butcher", "Dreadhorde Invasion", "Enter the God-Eternals", "Fblthp, the Lost", "Feather, the Redeemed", "Ignite the Beacon",
-	"Karn's Bastion", "Krenko, Tin Street Kingpin", "Living Twister", "Massacre Girl", "Mizzium Tank", "Mobilized District", "Narset's Reversal", "Neheb, Dreadhorde Champion",
-	"Oath of Kaya", "Parhelion II", "Planewide Celebration", "Ravnica at War", "Role Reversal", "Silent Submersible", "Single Combat",
-	"Solar Blaze", "Soul Diviner", "Spark Double", "Storrev, Devkarin Lich", "The Elderspell", "Time Wipe", "Tolsimir, Friend to Wolves",
-	"Tomik, Distinguished Advokist", "Vivien's Arkbow", "Widespread Brutality", "Awakening of Vitu-Ghazi", "Bioessence Hydra", "Blast Zone", "Bolas's Citadel", "Casualties of War", "Command the Dreadhorde", "Commence the Endgame",
-	"Deliver Unto Evil", "Dreadhorde Arcanist", "Dreadhorde Butcher", "Dreadhorde Invasion", "Enter the God-Eternals", "Fblthp, the Lost", "Feather, the Redeemed", "Ignite the Beacon",
-	"Karn's Bastion", "Krenko, Tin Street Kingpin", "Living Twister", "Massacre Girl", "Mizzium Tank", "Mobilized District", "Narset's Reversal", "Neheb, Dreadhorde Champion",
-	"Oath of Kaya", "Parhelion II", "Planewide Celebration", "Ravnica at War", "Role Reversal", "Silent Submersible", "Single Combat",
-	"Solar Blaze", "Soul Diviner", "Spark Double", "Storrev, Devkarin Lich", "The Elderspell", "Time Wipe", "Tolsimir, Friend to Wolves",
-	"Tomik, Distinguished Advokist", "Vivien's Arkbow", "Widespread Brutality", "Finale of Devastation", "Finale of Eternity", "Finale of Glory", "Finale of Promise", "Finale of Revelation",
-	"God-Eternal Bontu", "God-Eternal Kefnet", "God-Eternal Oketra", "God-Eternal Rhonas",
-	"Ilharg, the Raze-Boar", "Niv-Mizzet Reborn", "Roalesk, Apex Hybrid"
-	]
-	
-//Rare and Mythic Rare Planeswalkers
-let raresPW = [
-	"Ajani, the Greathearted", "Chandra, Fire Artisan", "Domri, Anarch of Bolas", "Jace, Weilder of Mysteries", "Karn the Great Creator", "Nissa, Who Shakes the World",
-	"Ral, Storm Conduit", "Sarkhan the Masterless", "Sorin, Vengeful Bloodlord", "Tamiyo, Collector of Tales", "Teferi, Time Raveler", "Ugin, the Ineffable", "Vivien, Champion of the Wilds", "Ajani, the Greathearted", "Chandra, Fire Artisan", "Domri, Anarch of Bolas", "Jace, Weilder of Mysteries", "Karn the Great Creator", "Nissa, Who Shakes the World",
-	"Ral, Storm Conduit", "Sarkhan the Masterless", "Sorin, Vengeful Bloodlord", "Tamiyo, Collector of Tales", "Teferi, Time Raveler", "Ugin, the Ineffable", "Vivien, Champion of the Wilds",
-	"Gideon Blackblade", "Liliana, Dreadhorde General", "Nicol Bolas, Dragon-God"
-	]
-
 //Random number generator
 function rng(max) {
   // here rand is from 1 to (max+1)
@@ -241,15 +136,21 @@ function fillPlayerPacks(cardPool) {
 	for (i=0;i<8;i++) {
 		for (j=0;j<3;j++) {
 			for (k=0;k<14;k++) {
-				console.log(cardPool[i][0][0])
-				//cardPool[i][j][k] = findCard(cardPool[i][j][k])
+				cardPool[i][j][k] = findCard(cardPool[i][j][k])
 			}
 		}
 	}
 }
 
+//function to search for card based on name
 function findCard(cardName) {
-	//function to search for card based on name
+	for (l=0;l<setWAR.length;l++)  {
+		if (setWAR[l].name == cardName) {
+			return setWAR[l];
+		}
+	}
+	console.log("No match for " + cardName)
+	return cardName;
 }
 
 /*function ScryfallRequest(playerPool) {
