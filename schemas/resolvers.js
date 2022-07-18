9f464ffbe04ad3aae11474e20495c808b30202f9
John Costello <jcostello711@gmail.com>
2022-07-17 21:18:56 -0400

    added login authentication using JWTs

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,17 +1,27 @@
-const { User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // logic here remains the same
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+    
+        return userData;
+      }
+    
+      throw new AuthenticationError('Not logged in');
+    }
   },
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
+      const token = signToken(user);
     
-      return user;
+      return { token, user };
     },
-
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
     
@@ -25,7 +35,8 @@ const resolvers = {
         throw new AuthenticationError('Incorrect credentials');
       }
     
-      return user;
+      const token = signToken(user);
+      return { token, user };
     }
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+// import authentication middleware
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -10,7 +12,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
